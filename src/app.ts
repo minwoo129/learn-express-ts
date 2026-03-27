feb43ea46eee5514f720295ab2ed4cedcb47dc9b
minwoo129 <key0129mw@naver.com>
2026-03-27 11:06:39 +0900

    기본 API 생성

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
